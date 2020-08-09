70caf3df25cff8232cd38213df6416a8f8b413ac
Nitin Singh <nitin1670@gmail.com>
2020-08-09 09:09:09 +0000

    Added Test Cases

---
+++ MainApp.Test/BasicTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MainApp.Test
+{
+    [TestClass]
+    public class BasicTest
+    {
+        [TestMethod]
+        public void AreEqual()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void AreNotEqual()
+        {
+            Assert.AreNotEqual(1, 2);
+        }
+    }
+}
